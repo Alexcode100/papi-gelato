d84ff71710af34709dcadd369904c73296efe4a2
Alexcode100 <89846069+Alexcode100@users.noreply.github.com>
2021-11-09 20:46:25 +0000

    Yes

--- Ijssalon.py
+++ Ijssalon.py
@@ -2,6 +2,11 @@
 import sys
 BakjeOfHoorntje = "bakje"
 Aantal_Bolletjes = 0
+Bolletje = 1
+Aardbei = 0
+Chocolade = 0
+Munt = 0
+Vanille = 0
 
 def Welkom(Aantal_Bolletjes):
     print('Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.')
@@ -14,8 +19,34 @@ def AantalBolletjes(Aantal_Bolletjes):
             break
         except(ValueError):
             print('Sorry, dat snap ik niet...')
+    SmaakBolletjes(Aantal_Bolletjes,Bolletje,Aardbei,Chocolade,Munt,Vanille)
+
+def SmaakBolletjes(Aantal_Bolletjes,Bolletje,Aardbei,Chocolade,Munt,Vanille):
+    Aantalx = Aantal_Bolletjes
+    while Aantalx >= 1:
+        SmaakBol = input('Welke smaak wilt u voor bolletje nummer '+str(Bolletje)+'? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?\n---> ')
+        Bolletje = Bolletje + 1
+        Aantalx = Aantalx - 1
+        if SmaakBol == "A":
+            Aardbei = Aardbei + 1
+        elif SmaakBol == "C":
+            Chocolade = Chocolade + 1
+        elif SmaakBol == "M":
+            Munt = Munt + 1
+        elif SmaakBol == "V":
+            Vanille = Vanille + 1
+        else:
+            Aantalx = Aantalx + 1
+            print('Sorry dat snap ik niet...')
+
+    print('Oke, u heeft dus de smaken:\n--------------\n'+str(Aardbei)+'x Aardbei\n'+str(Chocolade)+'x Chocolade\n'+str(Munt)+'x Munt\n'+str(Vanille)+'x Vanille\n--------------')
     Keuzes(Aantal_Bolletjes)
+
    
+
+
+
+
 def Keuzes(Aantal_Bolletjes):
     if Aantal_Bolletjes <= 3:
         HoornOfBakje = input('Wilt u deze '+str(Aantal_Bolletjes)+' bolletje(s) in A) een hoorntje of B) een bakje?\n---> ')
