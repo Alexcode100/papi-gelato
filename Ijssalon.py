00873430a8363273ec3e578162b4f23b46010db0
Alexcode100 <89846069+Alexcode100@users.noreply.github.com>
2021-11-09 22:06:07 +0000

    Yes

--- Ijssalon.py
+++ Ijssalon.py
@@ -8,6 +8,8 @@ Aardbei = 0
 Chocolade = 0
 Munt = 0
 Vanille = 0
+Hoorntje = 0
+Bakje = 0
 
 def Welkom(Aantal_Bolletjes):
     print('Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.')
@@ -41,30 +43,34 @@ def SmaakBolletjes(Aantal_Bolletjes,Bolletje,Aardbei,Chocolade,Munt,Vanille):
             print('Sorry dat snap ik niet...')
 
     print('Oke, u heeft dus de smaken:\n--------------\n'+str(Aardbei)+'x Aardbei\n'+str(Chocolade)+'x Chocolade\n'+str(Munt)+'x Munt\n'+str(Vanille)+'x Vanille\n--------------')
-    Keuzes(Aantal_Bolletjes)
+    Keuzes(Aantal_Bolletjes,Hoorntje,Bakje)
 
-def Keuzes(Aantal_Bolletjes):
+def Keuzes(Aantal_Bolletjes,Hoorntje,Bakje):
     if Aantal_Bolletjes <= 3:
         HoornOfBakje = input('Wilt u deze '+str(Aantal_Bolletjes)+' bolletje(s) in A) een hoorntje of B) een bakje?\n---> ')
         if HoornOfBakje == "A":
             BakjeOfHoorntje == "hoorntje"
+            Hoorntje = Hoorntje + 1
         elif HoornOfBakje == "B":
             BakjeOfHoorntje == "bakje"
+            Bakje = Bakje + 1
         else:
             print('Sorry, dat snap ik niet...')
-            Keuzes(Aantal_Bolletjes)
-        Uitslag(Aantal_Bolletjes)
+            Keuzes(Aantal_Bolletjes,Hoorntje,Bakje)
+        Uitslag(Aantal_Bolletjes,Hoorntje,Bakje)
     elif Aantal_Bolletjes <=8:
         print('Dan krijgt u van mij een bakje met '+ str(Aantal_Bolletjes)+' bolletjes')
-        MeerOfNietBestellen(Aantal_Bolletjes)
+        Bakje = Bakje + 1
+        print(Bakje)
+        Bonnetje(Aantal_Bolletjes,Hoorntje,Bakje)
 
     else:
         print('Sorry, zulke grote bakken hebben we niet')
         AantalBolletjes(Aantal_Bolletjes)
 
-def Uitslag(Aantal_Bolletjes):
+def Uitslag(Aantal_Bolletjes,Hoorntje,Bakje):
     print('Hier is uw '+str(BakjeOfHoorntje)+ ' met '+str(Aantal_Bolletjes)+' bolletje(s).')
-    MeerOfNietBestellen(Aantal_Bolletjes)
+    Bonnetje(Aantal_Bolletjes,Hoorntje,Bakje)
     
 def MeerOfNietBestellen(Aantal_Bolletjes):
     NogIets = input('Wilt u nog meer bestellen?\n---> ')
@@ -75,5 +81,25 @@ def MeerOfNietBestellen(Aantal_Bolletjes):
     else:
         print('Sorry dat snap ik niet...')
         MeerOfNietBestellen(Aantal_Bolletjes)
+
+def Bonnetje(Aantal_Bolletjes,Hoorntje,Bakje):
+    PrijsBolletjes = Aantal_Bolletjes*1.10
+    PrijsHorrentje = Hoorntje*1.25
+    PrijsBakje = Bakje*0.75
+    Totaal = PrijsBolletjes + PrijsHorrentje + PrijsBakje
+    print(Bakje)
+    print(Hoorntje)
+    print('------------[ Papi Gelato ]------------')
+    print(' ')
+    print("Bolletje('s)       "+str(Aantal_Bolletjes)+' x 1,10       = '+"{:.2f}".format(PrijsBolletjes))
+    if Hoorntje == 1:
+        print("Horrentje('s)   "+str(PrijsHorrentje)+' x 1,25       = '+"{:.2f}".format(PrijsHorrentje))
+    if Bakje == 1:
+        print("Bakje('s)       "+str(PrijsBakje)+' x 0,75       = '+"{:.2f}".format(PrijsBakje))
+    print('                                 ----- +')
+    print('Totaal                           = '+"{:.2f}".format(Totaal))
+    print(' ')
+    print('---------------------------------------')
+    MeerOfNietBestellen(Aantal_Bolletjes)
 
 Welkom(Aantal_Bolletjes)
