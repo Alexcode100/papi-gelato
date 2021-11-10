370df0806c4514bfd9b56aad99f52035893a0b9a
Alexcode100 <89846069+Alexcode100@users.noreply.github.com>
2021-11-10 08:16:24 +0000

    Yes

--- Ijssalon.py
+++ Ijssalon.py
@@ -6,7 +6,6 @@ Aantal_Bolletjes = 0
 Bolletje = 1
 Aardbei = 0
 Chocolade = 0
-Munt = 0
 Vanille = 0
 Hoorntje = 0
 Bakje = 0
@@ -24,34 +23,32 @@ def ParticulierOfZakelijk():
 
 def WelkomZakelijk(Liters):
     Liters = int(input('Hoeveel liter wilt u hebben?'))
-    LiterSmaak(Liter,Aardbei,Chocolade,Munt,Vanille,Liters)
+    LiterSmaak(Liter,Aardbei,Chocolade,Vanille,Liters)
 
-def LiterSmaak(Liter,Aardbei,Chocolade,Munt,Vanille,Liters):
+def LiterSmaak(Liter,Aardbei,Chocolade,Vanille,Liters):
     AantalLiters = Liters
     while AantalLiters >=1:
-        SmaakBol = input('Welke smaak wilt u voor de '+str(Liter)+'e liter? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?\n---> ')
+        SmaakBol = input('Welke smaak wilt u voor de '+str(Liter)+'e liter? A) Aardbei, C) Chocolade, of V) Vanille?\n---> ')
         if SmaakBol == "A":
             Aardbei = Aardbei + 1
         elif SmaakBol == "C":
             Chocolade = Chocolade + 1
-        elif SmaakBol == "M":
-            Munt = Munt + 1
         elif SmaakBol == "V":
             Vanille = Vanille + 1
         else:
             Aantalx = Aantalx + 1
-            print('Sorry dat snap ik niet...')
+            print('Sorry dat is geen optie die we aanbieden...')
         AantalLiters = AantalLiters - 1
-    print('Oke, u heeft dus de smaken:\n--------------\n'+str(Aardbei)+'x Aardbei\n'+str(Chocolade)+'x Chocolade\n'+str(Munt)+'x Munt\n'+str(Vanille)+'x Vanille\n--------------')
+    print('Oke, u heeft dus de smaken:\n--------------\n'+str(Aardbei)+'x Aardbei\n'+str(Chocolade)+'x Chocolade\n'+str(Vanille)+'x Vanille\n--------------')
     Doorgaan = input('Wilt u een bonnetje? (Ja/Nee)\n---> ')
     if Doorgaan == "Ja":
         BonnetjeZakelijk(Liters)
 
 def BonnetjeZakelijk(Liters):
-    btw = 9
+    btw = 6
     PrijsLiters = Liters * 9.80
     Totaal = PrijsLiters + btw
-    btw = PrijsLiters / 100 * 9
+    btw = PrijsLiters / 100 * 6
     print('------------[ Papi Gelato ]------------')
     print(' ')
     print("Liter('s)       "+str(Liters)+' x 9.80         = '+"{:.2f}".format(PrijsLiters))
@@ -70,28 +67,26 @@ def AantalBolletjes(Aantal_Bolletjes):
             Aantal_Bolletjes = int(input('Hoeveel bolletjes wilt u?\n---> '))
             break
         except(ValueError):
-            print('Sorry, dat snap ik niet...')
-    SmaakBolletjes(Aantal_Bolletjes,Bolletje,Aardbei,Chocolade,Munt,Vanille)
+            print('Sorry dat is geen optie die we aanbieden...')
+    SmaakBolletjes(Aantal_Bolletjes,Bolletje,Aardbei,Chocolade,Vanille)
 
-def SmaakBolletjes(Aantal_Bolletjes,Bolletje,Aardbei,Chocolade,Munt,Vanille):
+def SmaakBolletjes(Aantal_Bolletjes,Bolletje,Aardbei,Chocolade,Vanille):
     Aantalx = Aantal_Bolletjes
     while Aantalx >= 1:
-        SmaakBol = input('Welke smaak wilt u voor bolletje nummer '+str(Bolletje)+'? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?\n---> ')
+        SmaakBol = input('Welke smaak wilt u voor bolletje nummer '+str(Bolletje)+'? A) Aardbei, C) Chocolade, of V) Vanille?\n---> ')
         Bolletje = Bolletje + 1
         Aantalx = Aantalx - 1
         if SmaakBol == "A":
             Aardbei = Aardbei + 1
         elif SmaakBol == "C":
             Chocolade = Chocolade + 1
-        elif SmaakBol == "M":
-            Munt = Munt + 1
         elif SmaakBol == "V":
             Vanille = Vanille + 1
         else:
             Aantalx = Aantalx + 1
-            print('Sorry dat snap ik niet...')
+            print('Sorry dat is geen optie die we aanbieden...')
 
-    print('Oke, u heeft dus de smaken:\n--------------\n'+str(Aardbei)+'x Aardbei\n'+str(Chocolade)+'x Chocolade\n'+str(Munt)+'x Munt\n'+str(Vanille)+'x Vanille\n--------------')
+    print('Oke, u heeft dus de smaken:\n--------------\n'+str(Aardbei)+'x Aardbei\n'+str(Chocolade)+'x Chocolade\n'+str(Vanille)+'x Vanille\n--------------')
     Keuzes(Aantal_Bolletjes,Hoorntje,Bakje)
 
 def Keuzes(Aantal_Bolletjes,Hoorntje,Bakje):
@@ -104,7 +99,7 @@ def Keuzes(Aantal_Bolletjes,Hoorntje,Bakje):
             BakjeOfHoorntje == "bakje"
             Bakje = Bakje + 1
         else:
-            print('Sorry, dat snap ik niet...')
+            print('Sorry dat is geen optie die we aanbieden...')
             Keuzes(Aantal_Bolletjes,Hoorntje,Bakje)
         Toppings(Aantal_Bolletjes,ToppingKosten,DoorgaanOfNiet,Hoorntje,Bakje)
 
@@ -114,7 +109,7 @@ def Keuzes(Aantal_Bolletjes,Hoorntje,Bakje):
         Toppings(Aantal_Bolletjes,ToppingKosten,DoorgaanOfNiet,Hoorntje,Bakje)
         
     else:
-        print('Sorry, zulke grote bakken hebben we niet')
+        print('Sorry dat is geen optie die we aanbieden...')
         AantalBolletjes(Aantal_Bolletjes)
 
 
@@ -137,7 +132,7 @@ def Toppings(Aantal_Bolletjes,ToppingKosten,DoorgaanOfNiet,Hoorntje,Bakje):
                 else:
                     ToppingKosten = ToppingKosten + 0.90
             else:
-                print("Sorry dat snap ik niet...")
+                print("Sorry dat is geen optie die we aanbieden...")
                 Toppings(Aantal_Bolletjes,ToppingKosten,DoorgaanOfNiet,Hoorntje,Bakje)
             DoorgaanOfNiet = input('Wilt u nog een topping? (Ja/Nee)\n---> ')
     
@@ -155,11 +150,11 @@ def MeerOfNietBestellen(Aantal_Bolletjes):
     elif NogIets =="Nee":
         print('Bedankt en tot ziens!')
     else:
-        print('Sorry dat snap ik niet...')
+        print('Sorry dat is geen optie die we aanbieden...')
         MeerOfNietBestellen(Aantal_Bolletjes)
 
 def Bonnetje(Aantal_Bolletjes,Hoorntje,Bakje,ToppingKosten):
-    PrijsBolletjes = Aantal_Bolletjes*1.10
+    PrijsBolletjes = Aantal_Bolletjes*0.95
     PrijsHorrentje = Hoorntje*1.25
     PrijsBakje = Bakje*0.75
     Totaal = PrijsBolletjes + PrijsHorrentje + PrijsBakje + ToppingKosten
